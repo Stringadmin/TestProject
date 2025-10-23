107713e7defe2e00d609dcef0d1a9462168e41b6
Stringadmin <bysmj1213897@163.com>
2025-10-23 15:55:48 +0800

    修改后

--- app.js
+++ app.js
@@ -25,8 +25,5 @@ app.use(express.urlencoded({ extended: true }));
 // 路由
 app.use('/', upload.single('file'), indexRouter);
 
-// 启动服务器
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+// 作为 Vercel 函数导出（不要在此处 app.listen）
+module.exports = app;
